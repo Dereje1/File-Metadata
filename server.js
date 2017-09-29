a9eacbeda2c327d69b1ce47c6a784fc1c3de3bfa
Dereje1 <Dereje1>
2017-09-29 10:33:39 -0400

    remove multer upload option, so that files will never be written to disk

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let multer = require('multer') //required for parsing the file upload form
 let fs = require('fs')
 
 let app = express();
-let upload = multer({ dest: 'uploads/' }) // must set a file upload directory
+let upload = multer() // actually after reading docs the arguments to multer are optiona, and for this project I don't really need a directory as I am only outputing file info (example srgument for directory { dest: 'uploads/' })
 
 app.use(express.static('public'));
 
@@ -17,14 +17,8 @@ app.get("/", function (req, res) {
 
 //note .post for forms, also the upload.single filename must match the input type name and the the fileupload route must match the form "action" attribute
 app.post('/fileupload', upload.single('filetoupload'),function (req, res) {
- if(!req.file){//in case no file uploaded and submit button clicked send null
-   res.end(JSON.stringify({name:null,size:null}))
-   return;
- }
- fs.unlink(req.file.path,function(err,d){//deletes file once info is captured (works synchronously too)
-   if (err){res.end("Error deleting File")}
-   res.end(JSON.stringify({name:req.file.originalname,size:req.file.size}))
- })
+ //in case no file uploaded and submit button clicked send null otherwise...
+ return (!req.file) ? res.end(JSON.stringify({name:null,size:null})) : res.end(JSON.stringify({name:req.file.originalname,size:req.file.size}))
 })
 
 var listener = app.listen(process.env.PORT || 3000, function () {
